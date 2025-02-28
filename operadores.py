b8227901cb1f5d7b10e348a169f96a49ef462e06
Yennifer <yennimedrano02@gmail.com>
2025-02-28 09:05:48 -0500

    operaciones con la misma jerarquia

--- operadores.py
+++ operadores.py
@@ -4,10 +4,11 @@
     1./,*,%,
     2.+,-
     3.=
+    Si hay operaciones en el mismo nivel de jerarquia se resulve de izquierda a derecha
 '''
 a = 3
 b = 2
 c = 1
 
-y = a * b / 3 + c
+y = a ** 2 - a * b - 1 / 3 + c
 print(y)
