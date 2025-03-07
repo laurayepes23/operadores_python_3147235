2ccd8040e766317ea7c6875f615920b0dddfb4af
Yennifer <yennimedrano02@gmail.com>
2025-03-07 09:31:51 -0500

    operadores logicos

--- operadores_logicos.py
+++ operadores_logicos.py
@@ -35,5 +35,13 @@ JERARQUIA DEFINITIVA DE OPERADORES
 7.            AND
 8.             OR
 9.              =
-
+Nota: si hay operacones en el mismo nivel de jerarquia se resuelve de izquierda a derecha 
 '''
+
+
+op1 = False
+op2 = True
+op3 = False
+op4 = True
+
+resultado = not op1 and op2 or op3 and not op4
