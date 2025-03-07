4108b60cc5016714e747f785796a8e92f9b549d2
Yennifer <yennimedrano02@gmail.com>
2025-03-07 10:14:36 -0500

    ejemplos de condicionales

---
+++ condicionales.py
@@ -0,0 +1,22 @@
+'''
+CONDICIONALES
+expresion que al ser evaluada da como resultado un valor verdadero falso 
+en un a condicional debe haber un poerador relacionales o logicos 
+'''
+
+#EJEMPLO DE CONDICIONAL 
+a = 3
+b = 2
+c = 1
+d = 4
+resultado = (a ** 2 -b > c ** 2) and (( a * 3  + c ) /2 < d) or True
+print(resultado)
+
+
+#EJEMPLO
+x = 5
+y = 10
+z = 5
+
+resultado = (x == z + ( 8 / z ) ) and not ((y + 3) * ( 4 / ( z + 1 ))) == z
+print(resultado)
